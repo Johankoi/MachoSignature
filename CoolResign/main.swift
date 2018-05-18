15092de8773bbc3d386875a3823478b958325f09
hanxiaoqing <han@github.com>
2018-05-18 11:53:15 +0800

    commit  for support command resign

--- CoolResign/main.swift
+++ CoolResign/main.swift
@@ -5,50 +5,9 @@
 //  Created by hanxiaoqing on 2018/5/17.
 //  Copyright © 2018年 cheng. All rights reserved.
 //
-
-import Foundation
-
-print("begin test Option")
 //https://github.com/stupergenius/Bens-Log/blob/master/blog-projects/swift-command-line/btc.swift
 
-let inputOpt = Option(trigger:.mixed("i","inputfile-path"))
-let provisionOpt = Option(trigger:.mixed("p","provision-path"))
-let cerNameOpt = Option(trigger:.mixed("c","certificate-name"))
-let bundleIDOpt = Option(trigger:.mixed("b","bundleId"))
-let outputOpt = Option(trigger:.mixed("o","outputfile-path"))
-let helpOpt = Option(trigger:.mixed("h","help"))
-let parser = OptionParser(definitions:[inputOpt, provisionOpt, cerNameOpt, bundleIDOpt,outputOpt])
-
-let arguments = CommandLine.arguments
-let spliceArgs = arguments[1 ..< arguments.count]
-
-do {
-    let (options, rest) = try parser.parse(Array(spliceArgs))
-    
-    if options[inputOpt] != nil {
-        
-        parser.normalizeParameter
-        print("\(rest)")
-    }
-    
-    if options[provisionOpt] != nil {
-        print("\(rest)")
-    }
-    
-    if options[cerNameOpt] != nil {
-        print("\(rest)")
-    }
-    
-    
-    if options[helpOpt] != nil {
-        print(parser.helpStringForCommandName("optionTest"))
-    }
-} catch let OptionKitError.invalidOption(description: description) {
-    print(description)
-}
-
-
-
+import Foundation
 
 class CommandWorker: NSObject {
     
@@ -56,46 +15,13 @@ class CommandWorker: NSObject {
     var profliePath: String?
     var cerName: String?
     var outputPath: String?
+    var bundleID: String?
     
-    func work()  {
-        
-        let args = CommandLine.arguments
-//        let args = ["CoolResign", "-i", "/Users/hanxiaoqing/Documents/sdkTest_c.xcarchive", "-p", "/Users/hanxiaoqing/Desktop/SvnFolder/NewSDKFolder/provisioning/enterprise/DIS_ALL_NZK3GXHA6L.mobileprovision", "-c", "iPhone Distribution: Babeltime Inc.", "-o", "/Users/hanxiaoqing/Documents/sdkTest_c.ipa"]
-        
-        print("begin print args = \(args)")
-        
-        if args.count > 3 {
-            
-            if let pindex = args.index(of: "-i") {
-                inputPath = args[pindex + 1]
-            }
-            print("-i arg value \(inputPath)")
-            
-            
-            
-            if let pindex = args.index(of: "-p") {
-                profliePath = args[pindex + 1]
-            }
-            print("-p arg value \(profliePath)")
-            
-            
-            if let pindex = args.index(of: "-c") {
-                cerName = args[pindex + 1]
-            }
-            print("-c arg value \(cerName)")
-            
-            
-            if let pindex = args.index(of: "-o") {
-                outputPath = args[pindex + 1]
-            }
-            print("-o arg value \(outputPath)")
-            
-            let signer = CodeSigner()
-            signer.delegate = self
-            signer.sign(inputFile: inputPath!, provisioningFile: profliePath, newBundleID: "", newDisplayName: "", newVersion: "", newShortVersion: "", signingCertificate: cerName!, outputFile: outputPath!, openByTerminal: true)
-        }
+    func work() {
+        let signer = CodeSigner()
+        signer.delegate = self
+        signer.sign(inputFile: inputPath!, provisioningFile: profliePath, newBundleID: bundleID ?? "", newDisplayName: "", newVersion: "", newShortVersion: "", signingCertificate: cerName!, outputFile: outputPath!, openByTerminal: true)
     }
-
 }
 
 extension CommandWorker: CodeSignDelegate {
@@ -128,7 +54,77 @@ extension CommandWorker: CodeSignDelegate {
     }
 }
 
-//CommandWorker().work()
+
+let inputOpt = Option(trigger:.mixed("i","inputfile-path"))
+let provisionOpt = Option(trigger:.mixed("p","provision-path"))
+let cerNameOpt = Option(trigger:.mixed("c","certificate-name"))
+let bundleIDOpt = Option(trigger:.mixed("b","bundleId"))
+let outputOpt = Option(trigger:.mixed("o","outputfile-path"))
+
+let helpOpt = Option(trigger:.mixed("h","help"))
+let parser = OptionParser(definitions:[inputOpt, provisionOpt, cerNameOpt, bundleIDOpt,outputOpt,helpOpt])
+
+let arguments = CommandLine.arguments
+print("get all args: \(arguments)")
+
+let spliceArgs = arguments[1 ..< arguments.count]
+if spliceArgs.count == 0 { exit(0) }
+
+do {
+    let (options, rest) = try parser.parse(Array(spliceArgs))
+    
+    if options[helpOpt] != nil {
+        print(parser.helpStringForCommandName("show codesign important args"))
+        exit(0)
+    }
+    
+    let inputArgKeyCount = options.keys.count
+    print("input argkey count: \(inputArgKeyCount)")
+    
+    // require arg: -i -p -c -o, the "-h, -b" two args neednt required
+    if inputArgKeyCount != parser.definitions.count - 2 {
+        print("Input arg keys not enough, Please check !!!")
+        exit(0)
+    }
+    
+    guard rest.count == inputArgKeyCount else {
+        print("One or more arguments value not set, Please check !!!")
+        exit(0)
+    }
+    
+    let worker = CommandWorker()
+    rest.forEach { argValue in
+        let index = spliceArgs.index(of: argValue)
+        let argKey = spliceArgs[index! - 1]
+        
+        if inputOpt.matches(argKey) {
+            worker.inputPath = argValue
+        }
+        
+        if provisionOpt.matches(argKey) {
+            worker.profliePath = argValue
+        }
+        
+        if cerNameOpt.matches(argKey) {
+            worker.cerName = argValue
+        }
+        
+        if bundleIDOpt.matches(argKey) {
+            worker.bundleID = argValue
+        }
+        
+        if outputOpt.matches(argKey) {
+            worker.outputPath = argValue
+        }
+        worker.work()
+    }
+    
+    
+} catch let OptionKitError.invalidOption(description: description) {
+    print("OptionKit throw error: \n \(description)")
+}
+
+
 
 
 
